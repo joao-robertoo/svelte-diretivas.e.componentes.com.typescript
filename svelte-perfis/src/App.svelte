6375d0949101d0d608176a3bcb389001b3214bb2
J <joao.adsbrasil@gmail.com>
2025-06-25 22:32:25 -0300

    Primeiro componente

--- svelte-perfis/src/App.svelte
+++ svelte-perfis/src/App.svelte
@@ -1,34 +1,21 @@
 <script lang="ts">
-	let name: string = 'João';
-	let descricao = 'Esta é minha primeira aplicação Svelte!'
-	let src = 'https://upload.wikimedia.org/wikipedia/pt/b/b4/Corinthians_simbolo.png';
-	let alt = 'Imagem do Corinthians';
+	import Titulo from './components/Titulo.svelte';
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>{descricao}</p>
-	<img {src} {alt} height="300">
-</main>
+<div class="app">
+	<header>
+		<Titulo />
+	</header>
+</div>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
+	.app {
+		max-height: 100vh;
 	}
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
+	header {
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
 	}
 </style>
